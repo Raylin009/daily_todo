66d333ba9d0f26213a6d1737f7e181e9f153b1c1
Ray Lin <raylin009@gmail.com>
2019-08-01 18:34:12 -0700

    component files created, connected CurrTodoView.jsx to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import CurrTodoView from './components/CurrTodoView.jsx';
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      pastTodos: [],
-    }
+      // pastTodos:[],
+    };
   }
 
   render() {
     return (
       <div>
         <h1>App Component Established</h1>
+        <CurrTodoView />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/CurrTodoView.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class currTodoView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Current Todo View</h2>
+      </div>
+    );
+  }
+}
+
+export default currTodoView;
