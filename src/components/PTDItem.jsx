91ae99354a129b472d6f1413ed8308f9b78dc5aa
Ray Lin <raylin009@gmail.com>
2019-08-11 14:21:59 -0700

    delete list button randering on hover

--- src/components/PTDItem.jsx
+++ src/components/PTDItem.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 
-const PTDItem = ({ id, name }) => (
-  <div
-    key={id}
-    style={{ border: 'solid', margin: '5px' }}
-  >
-    <h3>{name}</h3>
-  </div>
-);
+// const PTDItem = ({ id, name }) => (
+//   <div
+//     key={id}
+//     style={{ border: 'solid', margin: '5px' }}
+//   >
+//     <h3>{name}</h3>
+//     <div hidden={false}>X</div>
+//   </div>
+// );
+
+class PTDItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideDelete: true,
+    };
+    this.toggleDelete = this.toggleDelete.bind(this);
+  }
+
+  toggleDelete() {
+    const { hideDelete } = this.state;
+    this.setState({
+      hideDelete: !hideDelete,
+    });
+  }
+
+  render() {
+    const { id, name } = this.props;
+    const { hideDelete } = this.state;
+
+    return (
+      <div
+        onMouseEnter={this.toggleDelete}
+        onMouseLeave={this.toggleDelete}
+        onClick={() => (console.log(`getItemsByListId ${id}`))}
+        key={id}
+        style={{ border: 'solid', margin: '5px' }}
+      >
+        <h3>{ name }</h3>
+        <div 
+          hidden={hideDelete}
+        >X</div>
+      </div>
+    );
+  }
+
+}
 
 export default PTDItem;
